ed0bd806ea086cc928314b8eee4b77e9744b2d33
Pallavi Nile <nile.p@northeastern.edu>
2025-04-09 17:02:02 -0400

    setup express server for render

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ function sum(a, b) {
   }
   module.exports = sum;
   
+  const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('CI/CD Demo App Deployed!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
